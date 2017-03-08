161b0e0b60a1165556d1695f537ebc125f9f081f
ivan <ivan.rabotyaga@gmail.com>
2017-03-08 18:55:37 +0300

    added alpabet js into precompiled assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,4 @@
 Rails.application.config.assets.precompile += %w( ra-keyboard.css )
 Rails.application.config.assets.precompile += %w( ra-keyboard.js )
 Rails.application.config.assets.precompile += %w( articles.css )
+Rails.application.config.assets.precompile += %w( alphabet.js )
